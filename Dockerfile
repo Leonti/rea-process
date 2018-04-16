56b96cc62857e4af10fe6e01fc43c18a84e6dbc2
Leonti Bielski <leonti.bielski@myob.com>
2018-04-16 12:22:08 +1000

    Upgrade haskell version

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN apk add --no-cache --update curl zip bash
 
 WORKDIR /root
 
-COPY .stack-work/install/x86_64-linux/lts-7.2/8.0.1/bin/rea-process rea-process
+COPY .stack-work/install/x86_64-linux/lts-11.0/8.2.2/bin/rea-process rea-process
 COPY stops.csv stops.csv
 COPY stores.csv stores.csv
 
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 version=$(date +"%y.%m.%d.%H.%M")
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-sudo docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:17.09.28.00.34
+sudo docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.04.04.03.12
 
 sudo docker build -t leonti/rea-process:$version .
 sudo docker push leonti/rea-process:$version
